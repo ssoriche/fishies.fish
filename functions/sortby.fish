a27e9c9d5bb83b3d565edb68f7805904f1a6ed8f
Shawn Sorichetti <ssoriche@users.noreply.github.com>
2026-03-27 11:16:16 -0400

    style: format all fish files with fish_indent
    Standardize formatting across all function files using fish_indent,
    the canonical Fish shell formatter. This ensures consistent indentation
    and style before introducing automated lint checks.

--- functions/sortby.fish
+++ functions/sortby.fish
@@ -3,21 +3,21 @@
 # SORTBY - sort by column name: sort tabular output by a specified column name
 # from the header row, preserving the header at the top
 function sortby
-  set -l name $argv[1]
-  if test -z "$name"
-    echo "Usage: sortby COLUMN_NAME" >&2
-    echo 'sortby - sort by column name; sorts tabular data by the specified column' >&2
-    return 1
-  end
+    set -l name $argv[1]
+    if test -z "$name"
+        echo "Usage: sortby COLUMN_NAME" >&2
+        echo 'sortby - sort by column name; sorts tabular data by the specified column' >&2
+        return 1
+    end
 
-  # Create temporary file
-  set -l tmpfile (mktemp)
+    # Create temporary file
+    set -l tmpfile (mktemp)
 
-  # Save all input to temp file
-  cat > $tmpfile
+    # Save all input to temp file
+    cat >$tmpfile
 
-  # Find column number
-  set -l col_num (head -n 1 $tmpfile | awk -v name=$name '
+    # Find column number
+    set -l col_num (head -n 1 $tmpfile | awk -v name=$name '
     {
       for(i=1; i<=NF; i++) {
         if($i==name) {
@@ -28,18 +28,18 @@ function sortby
     }
   ')
 
-  if test -z "$col_num"
-    echo "Error: Column \"$name\" not found" >&2
+    if test -z "$col_num"
+        echo "Error: Column \"$name\" not found" >&2
+        rm $tmpfile
+        return 1
+    end
+
+    # Print header
+    head -n 1 $tmpfile
+
+    # Sort data by column
+    tail -n +2 $tmpfile | sort -k $col_num
+
+    # Clean up
     rm $tmpfile
-    return 1
-  end
-
-  # Print header
-  head -n 1 $tmpfile
-
-  # Sort data by column
-  tail -n +2 $tmpfile | sort -k $col_num
-
-  # Clean up
-  rm $tmpfile
 end
